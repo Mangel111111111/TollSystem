35cd222ed3163751e29439fdf1d75e5887cbb063
Miguel Ángel García López <mangel111111111@gmail.com>
2025-02-06 13:11:42 +0100

    fix: add HasFactory trait

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Station;
 use App\Models\Vehicle;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Record extends Model
 {
--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Record;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Vehicle extends Model
 {
