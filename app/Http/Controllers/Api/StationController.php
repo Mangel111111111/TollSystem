d61cc2fa1ee8127d84b2802b2b46d70f76613ce2
Miguel Ángel García López <mangel111111111@gmail.com>
2025-02-06 12:56:17 +0100

    feat: add StationController and API routes

---
+++ app/Http/Controllers/Api/StationController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Station;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class StationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $stations = Station::with('records')->get();
+        return response()->json($stations, 200);
+    }
+
+    public function show(string $id)
+    {
+        $station = Station::find($id);
+
+        if (!$station) {
+            return response()->json(['message' => 'Station not found'], 404);
+        }
+
+        $station->load('records');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'city' => 'required|string',
+        ]);
+
+        $station = Station::create($data);
+        return response()->json($station, 201);
+    }
+
+    
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->validate([
+            'name'  => 'required|string',
+            'city'  => 'required|string',
+        ]);
+
+        $station = Station::find($id);
+
+        if (!$station) {
+            return response()->json(['message' => 'Station not found'], 404);
+        }
+
+        $station->update($data);
+
+        return response()->json($station, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $station = Station::find($id);
+
+        if (!$station) {
+            return response()->json(['message' => 'Station not found'], 404);
+        }
+
+        $station->delete();
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/stations', [StationController::class, 'index'])->name('apiIndexStations');
+Route::post('/stations', [StationController::class, 'store'])->name('apiStoreStations');
+Route::get('/stations/{id}', [StationController::class, 'show'])->name('apiShowStations');
+Route::put('/stations/{id}', [StationController::class, 'update'])->name('apiUpdateStations');
+Route::delete('/stations/{id}', [StationController::class, 'destroy'])->name('apiDestroyStations');
